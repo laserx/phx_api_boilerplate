a0101f655ac3523bddfd7004f96f421bad63dfc3
laserx <laserxie@gmail.com>
2019-12-23 15:37:24 +0800

    wip

--- lib/mix/tasks/pab.setup.ex
+++ lib/mix/tasks/pab.setup.ex
@@ -6,20 +6,92 @@ defmodule Mix.Tasks.Pab.Setup do
 
   @shortdoc "phx api boilerplate setup task"
 
+  @orig_module_name "PhxApiBoilerplate"
+  @orig_project_name "phx_api_boilerplate"
+
+  @scan_paths ~w[./lib/** ./priv/** ./test/** ./config/** mix.exs README.md]
+  @exclude_files ~w[/lib/mix/tasks/pab.setup.ex]
+
   def run(all_args) do
     {opts, args, _} =
       OptionParser.parse(all_args, switches: [module: :string], aliases: [m: :module])
 
     try do
-      c = Keyword.get(opts, :module)
-      ^c = String.capitalize(opts[:module])
+      case Keyword.get(opts, :module) do
+        nil -> nil
+        any -> true = String.starts_with?(any, String.first(any) |> String.upcase())
+      end
     rescue
-      _ -> raise ArgumentError, "module name must use Upcase"
+      _ ->
+        reraise(
+          ArgumentError,
+          "the first character of the module name must be capitalized",
+          __STACKTRACE__
+        )
     end
 
-    folder_name =
+    target_project_name =
       List.first(args) || raise(ArgumentError, "project name must provided") |> String.downcase()
 
-    IO.inspect([folder_name, opts])
+    target_module_name =
+      Keyword.get(opts, :module) ||
+        String.split(target_project_name, "_") |> Enum.map_join("", &String.capitalize(&1))
+
+    rename_module(target_module_name)
+    rename_project(target_project_name)
+  end
+
+  defp rename_project(targ, orig \\ @orig_project_name) do
+    Mix.shell().info("rename project from #{orig} to #{targ}")
+
+    paths = Enum.sort_by(scan_files(), &String.length/1, &>=/2)
+
+    for p <- paths do
+      case Path.split(p) do
+        any ->
+          if List.last(any)
+             |> String.contains?(orig) do
+            try do
+              path_tail = List.last(any) |> String.replace(orig, targ)
+              targ_path = List.delete_at(any, -1) |> List.insert_at(-1, path_tail) |> Path.join()
+              File.rename!(p, targ_path)
+            rescue
+              _ -> Mix.shell().info("rename project failed at #{p}")
+            end
+          end
+      end
+    end
+  end
+
+  defp rename_module(targ, orig \\ @orig_module_name) do
+    Mix.shell().info("rename module from #{orig} to #{targ}")
+
+    paths = scan_files()
+
+    for p <- paths do
+      case File.dir?(p) do
+        true ->
+          nil
+
+        false ->
+          unless Enum.member?(@exclude_files, p) do
+            contents =
+              File.stream!(p)
+              |> Enum.map(&String.replace(&1, orig, targ))
+              |> Enum.join("")
+
+            File.write!(p, contents)
+          end
+      end
+    end
+  end
+
+  defp scan_files() do
+    c =
+      for path <- @scan_paths do
+        Path.wildcard(path)
+      end
+
+    List.flatten(c)
   end
 end
